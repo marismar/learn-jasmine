ef7079dbea9ee1ff5401eecd6a2decaa3455c9c2
Marismar Costa <cmarismar@gmail.com>
2020-12-18 11:35:47 -0300

    first test with jasmine and karma

---
+++ src/app/main.spec.ts
@@ -0,0 +1,17 @@
+import { helloWorld } from './main';
+
+describe('Hello world', () => {
+  let expected = '';
+
+  beforeEach(() => {
+    expected = 'Hello world!';
+  });
+
+  afterEach(() => {
+    expected = '';
+  });
+
+  it('says hello', () => {
+    expect(helloWorld()).toEqual(expected);
+  });
+});
